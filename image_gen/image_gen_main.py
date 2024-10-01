df86bf7f024d86b74f81d01fb3e5dbf4cb4e5f3c
Eraser <113839336+Eraser-It-Re@users.noreply.github.com>
2024-10-01 14:41:14 +0900

    commit

--- image_gen/image_gen_main.py
+++ image_gen/image_gen_main.py
@@ -16,15 +16,25 @@ class Image_generator:
             except ValueError:
                 print('適切な整数を入力してください')
 
-        base_color = RGB_slider_GUI('下地の色').main()
-        print(base_color)
-        pattern_color1 = RGB_slider_GUI('柄の色').main()
+        # スライダーを呼び出し色を設定 決定を押したらスライダーのウィンドウを閉じる
+        base_color_slider = RGB_slider_GUI('下地の色')
+        base_color = base_color_slider.main()
+        base_color_slider.close_window()
+
+        pattern_color1_slider = RGB_slider_GUI('柄の色')
+        pattern_color1 = pattern_color1_slider.main()
+        pattern_color1_slider.close_window()
 
         if choice == 1:
             gen_image = Generate_check_pattern(base_color, pattern_color1)
             gen_image.process()
         elif choice == 2:
-            pattern_color2 = RGB_slider_GUI('2つ目の柄の色').main()
+
+            # ダイヤ柄を選んだ場合のみ柄の色2を選択させる
+            pattern_color2_slider = RGB_slider_GUI('2つ目の柄の色')
+            pattern_color2 = pattern_color1_slider.main()
+            pattern_color2_slider.close_window()
+
             gen_image = Generate_diamond_pattern(base_color, pattern_color1, pattern_color2)
             gen_image.process()
         elif choice == 3:
--- image_gen/rgb_slider.py
+++ image_gen/rgb_slider.py
@@ -6,13 +6,13 @@ class RGB_slider_GUI:
         self.description = description # 「何の色を選択させているか（下地・柄の色1・柄の色2）」を受け取り、説明をウィンドウ内に表示する
     def main(self):
         def confirm_color():
-            slider_window.quit()
+            self.slider_window.quit()
 
         # ウィンドウの作成
-        slider_window = tk.Tk()
-        slider_window.title('色選択')
-        slider_window.geometry('256x270')
-        slider_window.resizable(False, False) # ウィンドウサイズを固定
+        self.slider_window = tk.Tk()
+        self.slider_window.title('色選択')
+        self.slider_window.geometry('256x270')
+        self.slider_window.resizable(False, False) # ウィンドウサイズを固定
 
 
 
@@ -40,36 +40,41 @@ class RGB_slider_GUI:
 
 
         #
-        description_text = tk.Label(slider_window, text = f'{self.description}を選択してください')
+        description_text = tk.Label(self.slider_window, text = f'{self.description}を選択してください')
         description_text.place(x = 0, y = 10)
 
         # スライダーの作成
-        scale_r = tk.Scale(slider_window, from_ = 255, to_ = 0, label='R', command=on_scale_r)
+        scale_r = tk.Scale(self.slider_window, from_ = 255, to_ = 0, label='R', command=on_scale_r)
         scale_r.place(x = 0, y = 34)
-        scale_g = tk.Scale(slider_window, from_ = 255, to_ = 0, label='G', command=on_scale_g)
+        scale_g = tk.Scale(self.slider_window, from_ = 255, to_ = 0, label='G', command=on_scale_g)
         scale_g.place(x = 85, y = 34)
-        scale_b = tk.Scale(slider_window, from_ = 255, to_ = 0, label='B', command=on_scale_b)
+        scale_b = tk.Scale(self.slider_window, from_ = 255, to_ = 0, label='B', command=on_scale_b)
         scale_b.place(x = 170, y = 34)
 
         # キャンバスの作成
-        label = tk.Label(slider_window, text = '色見本')
+        label = tk.Label(self.slider_window, text = '色見本')
         label.place(x = 0, y = 144)
-        canvas = tk.Canvas(slider_window, width=100, height=100, bg='#000000')
+        canvas = tk.Canvas(self.slider_window, width=100, height=100, bg='#000000')
         canvas.place(x = 0, y = 164)
 
         # 決定ボタン
-        confirm_button = tk.Button(slider_window, text='決定', command=confirm_color)
+        confirm_button = tk.Button(self.slider_window, text='決定', command=confirm_color)
         confirm_button.place (x = 210, y = 234)
 
         # ウィンドウを最前面に表示する
-        slider_window.attributes('-topmost', True)
+        self.slider_window.attributes('-topmost', True)
 
         # ウィンドウの起動
-        slider_window.mainloop()
+        self.slider_window.mainloop()
 
         # 終了時、ユーザーに指定されたRGB値を返す
         rgb = (scale_r.get(), scale_g.get(), scale_b.get())
         return rgb
+    
+    # ウィンドウを閉じる
+    def close_window(self):
+        self.slider_window.destroy()
+
 
 # ui = RGB_slider_GUI
 # print(ui.main())
